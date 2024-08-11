c0e2fc1401322ee6b841a3edb2a2c0cd695ac4c4
Valtrix3 <168859977+Valtrix3@users.noreply.github.com>
2024-08-11 03:19:42 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,19 +11,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "237698581946"
-global.ownername = "🐉 Limule Solitarus"
+global.ownernomer = "2250575302404"
+global.ownername = "valtrix"
 global.ytname = "YT: Deaethgun635"
 global.socialm = "GitHub: Limule3650"
 global.location = "India, Rampur, city"
 
-global.ownernumber = '237698581946'  //creator number
-global.ownername = '🐉 Limule Solitarus' //owner name
+global.ownernumber = '2250575302404'  //creator number
+global.ownername = 'valtrix' //owner name
 global.botname = 'DEATH RAPHAEL-BUG' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By'
-global.author = 'Limule Solitarus\n\nContact: +237698581946'
+global.packname = 'valtrix'
+global.author = 'valtrix'
 
 //console view/theme
 global.themeemoji = '🪀'
@@ -38,11 +38,11 @@ global.prefa = ['','!','.','#','&','•']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = true //auto view status/story
+global.autoswview = false //auto view status/story
 
 //menu type 
 //v1 is image menu, 
